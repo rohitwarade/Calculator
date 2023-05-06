22b31bc0a1e310f5812c4b4ca762d4c69edbc8e2
rohitwarade <waraderp@gmail.com>
2023-05-06 23:07:19 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let outputscreen = document.getElementById("output-screen");
+
+function display(num){
+    outputscreen.value += num;
+}
+
+function Calculate(){
+    try{
+        outputscreen.value=eval(outputscreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid input")
+    }
+}
+
+function Clear(){
+    outputscreen.value = "";
+}
+
+function del(){
+    outputscreen.value = outputscreen.value.slice(0,-1);
+}
